9d8bdcb5994e434d87663a25147622bc7f0ea2a6
akulah91 <akulah91@gmail.com>
2019-02-18 07:04:08 +0800

    Updates

---
+++ precache-manifest.2bcf691a13ef54790a363bcc3236b3c8.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/kongsisusu/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "eb289f1f2d9546e34f7d",
+    "url": "/kongsisusu/static/css/main.82b771a5.chunk.css"
+  },
+  {
+    "revision": "cbba169936a70d4b575c",
+    "url": "/kongsisusu/static/js/runtime~main.cbba1699.js"
+  },
+  {
+    "revision": "958cf9c7bc4ac7e609694104eb6ac21e",
+    "url": "/kongsisusu/static/media/depositphotos_114716608-stock-illustration-presentation-template-what-to-eat.958cf9c7.jpg"
+  },
+  {
+    "revision": "96a6d7f65bd9ce95d0d1",
+    "url": "/kongsisusu/static/js/2.96a6d7f6.chunk.js"
+  },
+  {
+    "revision": "5a8c73c896d793d3bb3b81b8b669da5a",
+    "url": "/kongsisusu/static/media/women_group.5a8c73c8.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/kongsisusu/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/kongsisusu/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/kongsisusu/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/kongsisusu/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/kongsisusu/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/kongsisusu/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/kongsisusu/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/kongsisusu/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "eb289f1f2d9546e34f7d",
+    "url": "/kongsisusu/static/js/main.eb289f1f.chunk.js"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/kongsisusu/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/kongsisusu/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/kongsisusu/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/kongsisusu/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/kongsisusu/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/kongsisusu/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/kongsisusu/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "9254b9ea68eca665627f4aec16527d7a",
+    "url": "/kongsisusu/static/media/1471467.9254b9ea.jpg"
+  },
+  {
+    "revision": "b8e88f2f74a877f7e7eb962d9f6efc96",
+    "url": "/kongsisusu/static/media/Breastfeeding Support.b8e88f2f.png"
+  },
+  {
+    "revision": "29d6885e0131f8e2d409d7fbfecd0a31",
+    "url": "/kongsisusu/static/media/Picture1.29d6885e.png"
+  },
+  {
+    "revision": "51185ed0aff5b1f85e5937ee7134081c",
+    "url": "/kongsisusu/static/media/kebaikan-susu-ibu.51185ed0.jpg"
+  },
+  {
+    "revision": "1a42b59ae7229ee08c3fd93834ad5987",
+    "url": "/kongsisusu/static/media/Klinik Ibu Susuan.1a42b59a.jpg"
+  },
+  {
+    "revision": "96a6d7f65bd9ce95d0d1",
+    "url": "/kongsisusu/static/css/2.41775ac1.chunk.css"
+  },
+  {
+    "revision": "61419a94be0069e19efc51954d980885",
+    "url": "/kongsisusu/index.html"
+  }
+];
