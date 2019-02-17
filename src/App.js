56adc95a3f48744981367e8c2b890a0616b942be
akulah91 <akulah91@gmail.com>
2019-02-18 06:54:59 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router';
+import 'semantic-ui-css/semantic.min.css';
+import './assets/css/App.css';
+import HomepageLayout from './screens/HomepageLayout';
+import About from './screens/About';
+import Faq from './screens/Faq';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Switch>
+      <Route exact path="/" component={HomepageLayout}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/faq" component={Faq}/>
+    </Switch>
     );
   }
 }
